32f038ddc12f6f5eeda83a35cb1639241cd57e55
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-10-17 19:56:25 -0400

    Updated from Brython Server: 10/17/2018 7:56:25 PM

--- program.py
+++ program.py
@@ -1,1 +1,15 @@
+from math import *
 
+n = int(input("Numerator? "))
+d = int(input("Denominator? "))
+
+common_factor = 1
+for i in range(min(abs(n), abs(d)), 1, -1):
+     if n%i == 0 and d%i == 0:
+         common_factor = i
+         break
+
+n = n/i
+d = d/i
+     
+print("Simplified fraction is {0}/{1}".format(int(n), int(d)))

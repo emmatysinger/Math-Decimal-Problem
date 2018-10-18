9d4217b8732bd829b4c297d2598adb9434f4e0a6
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-10-17 21:56:02 -0400

    Updated from Brython Server: 10/17/2018 9:56:02 PM

--- program.py
+++ program.py
@@ -1,15 +1,36 @@
 from math import *
-
-n = int(input("Numerator? "))
-d = int(input("Denominator? "))
-
-common_factor = 1
-for i in range(min(abs(n), abs(d)), 1, -1):
-     if n%i == 0 and d%i == 0:
-         common_factor = i
-         break
-
-n = n/i
-d = d/i
-     
-print("Simplified fraction is {0}/{1}".format(int(n), int(d)))
+for d in range(2,15)
+    # n = int(input("Numerator? "))
+    # d = int(input("Denominator? "))
+    
+    common_factor = 1
+    for i in range(min(abs(n), abs(d)), 1, -1):
+         if n%i == 0 and d%i == 0:
+             common_factor = i
+             break
+    
+    n = n/common_factor
+    d = d/common_factor
+    non_repeating = [10, 5, 2]
+    # w = { 2:[1,0], 3:[0,1], 5:[1,0], 7:[0,6], 9:[0,1], 11:[0,2]}
+    # print("Simplified fraction is {0}/{1}".format(int(n), int(d)))
+    factors = []
+    
+    for k in non_repeating:
+        while d%k == 0:
+            factors.append(k)
+            d /= k
+        if k == 2:
+            factors.append(int(d))
+    
+    
+    for i in factors:
+        if i != 2 and i != 5:
+            while True:
+                k=0
+                if (10^k-1)%1 == 0:
+                    r = k
+                    break
+                k += 1
+    
+    print(r)
